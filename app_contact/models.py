5205d66f723621b61a0a4ac09d327eeda2e5831d
jonathan <j.brutschin@gmail.com>
2021-05-04 17:17:39 +0200

    add app_contact models

---
+++ app_contact/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+CONTACT_TYPE_CHOICES = (
+    ('private', 'Privat'),
+    ('business', 'Business'),
+)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=256)
+    first_name = models.CharField(max_length=256, blank=True)
+    type = models.CharField(max_length=20, default='private')
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=256)
+    zip = models.CharField(max_length=10)
+    city = models.CharField(max_length=256)
+    country = models.CharField(max_length=2)
