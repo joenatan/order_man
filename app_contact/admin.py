ecad063af4039618c95c556e25c90d3b7d3c9e2c
jonathan <j.brutschin@gmail.com>
2021-05-04 17:20:23 +0200

    add contact to admin area

--- app_contact/admin.py
+++ app_contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact, Address
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type']
+    list_filter = ['type']
+    search_fields = ['name', 'first_name']
