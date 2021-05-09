9f1efe4a29a896737d537673adbf43aee1990c31
jonathan <j.brutschin@gmail.com>
2021-05-09 19:40:41 +0200

    add address endpoint

--- app_contact/serializers.py
+++ app_contact/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from .models import Contact
+from .models import Contact, Address
 
 
 class ContactSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Contact
+        fields = '__all__'
+
+
+class AddressSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Address
         fields = '__all__'
--- app_contact/views.py
+++ app_contact/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from .models import Contact
-from .serializers import ContactSerializer
+from .models import Contact, Address
+from .serializers import ContactSerializer, AddressSerializer
 
 
 class ContactApiView(viewsets.ModelViewSet):
     serializer_class = ContactSerializer
     queryset = Contact.objects.all()
+
+
+class AddressApiView(viewsets.ModelViewSet):
+    serializer_class = AddressSerializer
+    queryset = Address.objects.all()
